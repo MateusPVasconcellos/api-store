36318219c93ec243e052a8acd427a82606d65d98
Mateus Vasconcellos <mateusdepaula3@gmail.com>
2022-12-15 13:23:32 -0300

    Implementing profile routes

--- src/modules/users/routes/profile.routes.ts
+++ src/modules/users/routes/profile.routes.ts
@@ -1,12 +1,13 @@
 import { Router } from 'express';
 import ProfileController from '../controllers/ProfileController';
 import isAuthenticated from '@shared/http/middlewares/isAuthenticated';
+import { bodyUpdateProfileDto } from '../dto/body-profile-update.dto';
 
 const profileRouter = Router();
 profileRouter.use(isAuthenticated);
 
 profileRouter.get('/', ProfileController.show);
 
-profileRouter.put('/', ProfileController.update);
+profileRouter.put('/', bodyUpdateProfileDto(), ProfileController.update);
 
 export default profileRouter;
