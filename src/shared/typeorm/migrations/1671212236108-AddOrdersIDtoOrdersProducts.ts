68f0d749559837c318d74ec2d752a603c0081b1f
Mateus Vasconcellos <mateusdepaula3@gmail.com>
2022-12-16 15:36:17 -0300

    Creatin migrations and Order entity

---
+++ src/shared/typeorm/migrations/1671212236108-AddOrdersIDtoOrdersProducts.ts
@@ -0,0 +1,37 @@
+import {
+  MigrationInterface,
+  QueryRunner,
+  TableColumn,
+  TableForeignKey,
+} from 'typeorm';
+
+export class AddOrdersIDtoOrdersProducts1671212236108
+  implements MigrationInterface
+{
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.addColumn(
+      'orders_products',
+      new TableColumn({
+        name: 'order_id',
+        type: 'uuid',
+        isNullable: true,
+      }),
+    );
+
+    await queryRunner.createForeignKey(
+      'orders_products',
+      new TableForeignKey({
+        name: 'OrdersProductsOrder',
+        columnNames: ['order_id'],
+        referencedTableName: 'orders',
+        referencedColumnNames: ['id'],
+        onDelete: 'SET NULL',
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey('orders_products', 'OrdersProductsOder');
+    await queryRunner.dropColumn('orders_products', 'order_id');
+  }
+}
