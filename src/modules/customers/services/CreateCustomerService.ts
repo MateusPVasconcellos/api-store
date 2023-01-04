07c48d817c947973358c9213a985cd63e6021649
Mateus Vasconcellos <mateusdepaula3@gmail.com>
2023-01-04 09:19:36 -0300

    Adding domain layer to Customer

--- src/modules/customers/services/CreateCustomerService.ts
+++ src/modules/customers/services/CreateCustomerService.ts
@@ -2,16 +2,12 @@ import RedisCache from '@shared/cache/RedisCache';
 import { RedisCustomersKeys } from '@shared/enums/redis-customers-keys';
 import AppError from '@shared/errors/AppError';
 import httpStatus from 'http-status-codes';
+import { ICreateCustomer } from '../domain/models/ICreateCustomer';
 import Customer from '../infra/typeorm/entities/Customer';
 import { CustomersRepository } from '../infra/typeorm/repositories/CustomersRepository';
 
-interface IRequest {
-  name: string;
-  email: string;
-}
-
 class CreateCustomerService {
-  public async execute({ name, email }: IRequest): Promise<Customer> {
+  public async execute({ name, email }: ICreateCustomer): Promise<Customer> {
     const customerExist = await CustomersRepository.findByEmail(email);
 
     if (customerExist) {
